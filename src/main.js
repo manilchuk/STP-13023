e06e87fd704c5f54f135b3387a6008c29b62de3c
Valentyn <manilchuk.v@gmail.com>
2026-07-07 21:44:13 +0300

    fix header.css

--- src/main.js
+++ src/main.js
@@ -1,42 +1,50 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-menu-open]'),
-    closeModalBtn: document.querySelector('[data-menu-close]'),
-    modal: document.querySelector('[data-menu]'),
+    openBtn: document.querySelector('[data-menu-open]'),
+    closeBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
+    burger: document.querySelector('[data-burger]'),
+    menuLinks: document.querySelectorAll('[data-menu] .menu-nav-link'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openBtn.addEventListener('click', toggleMenu);
+  refs.closeBtn.addEventListener('click', toggleMenu);
 
   // Закриття по кліку на бекдроп
-  refs.modal.addEventListener('click', e => {
-    if (e.target === refs.modal) {
-      toggleModal();
+  refs.menu.addEventListener('click', e => {
+    if (e.target === refs.menu) {
+      toggleMenu();
     }
   });
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-open');
-    document.body.classList.toggle('no-scroll');
-  }
-
-  document.querySelectorAll('.menu-nav-link').forEach(link => {
+  // Закриття по кліку на пункт меню
+  refs.menuLinks.forEach(link => {
     link.addEventListener('click', () => {
-      if (refs.modal.classList.contains('is-open')) {
-        toggleModal();
+      if (refs.menu.classList.contains('is-open')) {
+        toggleMenu();
       }
     });
   });
 
+  function toggleMenu() {
+    const isOpen = refs.menu.classList.toggle('is-open');
+
+    document.body.classList.toggle('no-scroll', isOpen);
+
+    refs.burger.classList.toggle('is-hidden', isOpen);
+  }
+
   // burger animation
-  const hotspot = document.querySelector('.hero-hotspot');
-  const burger = document.querySelector('.hero-burger');
+  const hotspot = document.querySelector('[data-hero-hotspot]');
+  const burgerAnimation = document.querySelector('[data-hero-burger]');
 
-  hotspot.addEventListener('mouseenter', () => {
-    burger.classList.add('show');
-  });
+  if (hotspot && burgerAnimation) {
+    hotspot.addEventListener('mouseenter', () => {
+      burgerAnimation.classList.add('show');
+    });
 
-  hotspot.addEventListener('mouseleave', () => {
-    burger.classList.remove('show');
-  });
+    hotspot.addEventListener('mouseleave', () => {
+      burgerAnimation.classList.remove('show');
+    });
+  }
 })();
