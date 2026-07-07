c9d76c3a8e4606b08ef4c4d26707a4884e81863a
Valentyn <manilchuk.v@gmail.com>
2026-07-07 23:12:50 +0300

    fix main.js

--- src/main.js
+++ src/main.js
@@ -1,50 +1,41 @@
-(() => {
-  const refs = {
-    openBtn: document.querySelector('[data-menu-open]'),
-    closeBtn: document.querySelector('[data-menu-close]'),
-    menu: document.querySelector('[data-menu]'),
-    burger: document.querySelector('[data-burger]'),
-    menuLinks: document.querySelectorAll('[data-menu] .menu-nav-link'),
-  };
+const openBtnEl = document.querySelector('[data-menu-open]');
+const closeBtnEl = document.querySelector('[data-menu-close]');
+const menuEl = document.querySelector('[data-menu]');
+const burgerEl = document.querySelector('[data-burger]');
+const menuLinksEl = document.querySelectorAll('[data-menu-link]');
 
-  refs.openBtn.addEventListener('click', toggleMenu);
-  refs.closeBtn.addEventListener('click', toggleMenu);
+openBtnEl.addEventListener('click', toggleMenu);
+closeBtnEl.addEventListener('click', toggleMenu);
 
-  // Закриття по кліку на бекдроп
-  refs.menu.addEventListener('click', e => {
-    if (e.target === refs.menu) {
-      toggleMenu();
-    }
+menuEl.addEventListener('click', e => {
+  if (e.target === menuEl) {
+    toggleMenu();
+  }
+});
+
+menuLinksEl.forEach(link => {
+  link.addEventListener('click', toggleMenu);
+});
+
+function toggleMenu() {
+  const isOpen = menuEl.dataset.visible === 'open';
+
+  menuEl.dataset.visible = isOpen ? 'close' : 'open';
+  burgerEl.dataset.visible = isOpen ? 'show' : 'hide';
+
+  document.body.classList.toggle('no-scroll', !isOpen);
+}
+
+// анімація бургера
+const hotspotEl = document.querySelector('[data-hero-hotspot]');
+const burgerAnimationEl = document.querySelector('[data-hero-burger]');
+
+if (hotspotEl && burgerAnimationEl) {
+  hotspotEl.addEventListener('mouseenter', () => {
+    burgerAnimationEl.dataset.animation = 'show';
   });
 
-  // Закриття по кліку на пункт меню
-  refs.menuLinks.forEach(link => {
-    link.addEventListener('click', () => {
-      if (refs.menu.classList.contains('is-open')) {
-        toggleMenu();
-      }
-    });
+  hotspotEl.addEventListener('mouseleave', () => {
+    burgerAnimationEl.dataset.animation = 'hide';
   });
-
-  function toggleMenu() {
-    const isOpen = refs.menu.classList.toggle('is-open');
-
-    document.body.classList.toggle('no-scroll', isOpen);
-
-    refs.burger.classList.toggle('is-hidden', isOpen);
-  }
-
-  // burger animation
-  const hotspot = document.querySelector('[data-hero-hotspot]');
-  const burgerAnimation = document.querySelector('[data-hero-burger]');
-
-  if (hotspot && burgerAnimation) {
-    hotspot.addEventListener('mouseenter', () => {
-      burgerAnimation.classList.add('show');
-    });
-
-    hotspot.addEventListener('mouseleave', () => {
-      burgerAnimation.classList.remove('show');
-    });
-  }
-})();
+}
